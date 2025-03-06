d6488537f12fe3001291c3706ee6729d1da027a2
Vijay K. <sharmavijay86@gmail.com>
2025-03-06 15:32:17 +0530

    Create main.tf

---
+++ tf-workspace/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.89.0"
+    }
+  }
+  backend "s3" {
+    bucket = "kr-tfstate-bucket"  # Replace with your bucket name
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-deploy-vpc"
+  }
+}
